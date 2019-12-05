df2e7fc6870f83abf42f6d66a4f440b54626de7c
mielwyn <melwyn2021@pingry.org>
2019-12-05 10:33:06 -0500

    Revert "Late night work"
    This reverts commit 4b8080742402126f718303a39ce3a8626493c9d9.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DriveMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/DriveMain.java
@@ -22,41 +22,29 @@ import java.util.List;
 
 @TeleOp(group="drive")
 public class DriveMain extends LinearOpMode {
-    double dt;
-    double last;
-    double now;
-    Pose2d currentPose = new Pose2d(0,0,0);
     @Override
     public void runOpMode() throws InterruptedException{
         MecanumDriveBaseOptimized drive = new MecanumDriveBaseOptimized(hardwareMap);
         waitForStart();
+        double dt;
+        double last;
+        double now;
+        last = System.currentTimeMillis();
+        Pose2d currentPose = new Pose2d(0,0,drive.getRawExternalHeading());
+
         double posIn = .1;
         double posOut = .9;
-        ExpansionHubMotor winch = hardwareMap.get(ExpansionHubMotor.class,"winch");
-        winch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        winch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        last = System.currentTimeMillis();
         while(!isStopRequested()) {
             Vector2d translationRotatedForFieldCentric = new Vector2d(-gamepad1.left_stick_x, -gamepad1.left_stick_y).rotated(drive.getRawExternalHeading());
-            drive.setDrivePower(new Pose2d(translationRotatedForFieldCentric.getY(),translationRotatedForFieldCentric.getX(), -gamepad1.right_stick_x/4));
+            drive.setDrivePower(new Pose2d(translationRotatedForFieldCentric.getY(),translationRotatedForFieldCentric.getX(), -gamepad1.right_stick_x/2));
             List<Double> wheelVelocities = drive.getWheelVelocities();
             now = System.currentTimeMillis();
             dt = now - last;
             dt = dt / ((double) 1000);
-            Pose2d robotFrameVelocity = MecanumKinematics.wheelToRobotVelocities(wheelVelocities, DriveConstants.TRACK_WIDTH, DriveConstants.WHEEL_BASE);
-            Pose2d robotFrameVelocityRotated = new Pose2d(new Vector2d(robotFrameVelocity.getX(),robotFrameVelocity.getY()).rotated(drive.getRawExternalHeading()),robotFrameVelocity.getHeading());
-            Pose2d poseDelta = robotFrameVelocityRotated.times(dt);
+            Pose2d poseDelta = MecanumKinematics.wheelToRobotVelocities(wheelVelocities, DriveConstants.TRACK_WIDTH, DriveConstants.WHEEL_BASE).times(dt);
+            poseDelta = new Pose2d(new Vector2d(poseDelta.getX(),poseDelta.getY()).rotated(drive.getRawExternalHeading()),poseDelta.getHeading());
             currentPose = currentPose.plus(poseDelta);
             last = now;
-            if (gamepad1.a){
-                winch.setPower(-.5);
-            }
-            else if (gamepad1.b){
-                winch.setPower(.5);
-            }
-            else{
-                winch.setPower(0);
-            }
             telemetry.addData("Wheel Velocities",wheelVelocities);
             telemetry.addData("Pose Delta:",poseDelta);
             telemetry.addData("Current Pose:",currentPose);
